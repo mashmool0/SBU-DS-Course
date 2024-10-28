65749305362c2beb328b55e7510724d21a76d5ee
Amir Mashayekhi <ap4games2017@gmail.com>
2024-10-28 12:04:53 +0330

    test linked list

--- test_linkedlist.py
+++ test_linkedlist.py
@@ -8,3 +8,66 @@ linded_list.InsertAtEnd('b')
 linded_list.InsertAtEnd('c')
 linded_list.InsertAtIndex('h',1)
 
+# create a new linked list
+llist = LinkedList()
+
+# add nodes to the linked list
+llist.insertAtEnd('a')
+llist.insertAtEnd('b')
+llist.insertAtBegin('c')
+llist.insertAtEnd('d')
+llist.insertAtIndex('g', 2)
+
+# print the linked list
+print("Node Data")
+llist.printLL()
+
+# remove a nodes from the linked list
+print("\nRemove First Node")
+llist.remove_first_node()
+print("Remove Last Node")
+llist.remove_last_node()
+print("Remove Node at Index 1")
+llist.remove_at_index(1)
+
+# print the linked list again
+print("\nLinked list after removing a node:")
+llist.printLL()
+
+print("\nUpdate node Value")
+llist.updateNode('z', 0)
+llist.printLL()
+
+print("\nSize of linked list :", end=" ")
+print(llist.sizeOfLL())# create a new linked list
+llist = LinkedList()
+
+# add nodes to the linked list
+llist.insertAtEnd('a')
+llist.insertAtEnd('b')
+llist.insertAtBegin('c')
+llist.insertAtEnd('d')
+llist.insertAtIndex('g', 2)
+
+# print the linked list
+print("Node Data")
+llist.printLL()
+
+# remove a nodes from the linked list
+print("\nRemove First Node")
+llist.remove_first_node()
+print("Remove Last Node")
+llist.remove_last_node()
+print("Remove Node at Index 1")
+llist.remove_at_index(1)
+
+# print the linked list again
+print("\nLinked list after removing a node:")
+llist.printLL()
+
+print("\nUpdate node Value")
+llist.updateNode('z', 0)
+llist.printLL()
+
+print("\nSize of linked list :", end=" ")
+print(llist.sizeOfLL())
