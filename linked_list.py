b3f0b181d41fcc77ac134d662947445586e845b6
Amir Mashayekhi <ap4games2017@gmail.com>
2024-10-28 12:04:09 +0330

    add size of all method for linked list class

--- linked_list.py
+++ linked_list.py
@@ -126,11 +126,20 @@ class LinkedList :
         
         while (current_node != None ) : 
             print(current_node.data) 
-            
             current_node = current_node.next 
-            
+    
+    def sizeOfLL(self):
+        size = 0
+        if(self.head):
+            current_node = self.head
+            while(current_node):
+                size = size+1
+                current_node = current_node.next
+            return size
+        else:
+            return 0
         
             
         
-                    
+            
     
