8d4b7c1aa42e4ca45160bb49f6abad536af5c7d2
Amir Mashayekhi <ap4games2017@gmail.com>
2024-11-09 17:05:27 +0330

    filimo

--- HW2/filimo_matrice.py
+++ HW2/filimo_matrice.py
@@ -5,18 +5,6 @@ twoD_list = [
     [0,0,1,1,1,0,0,0]
 ]
 
-# Algorithm for Checking movies user  Watch
-
-
-# print(f'choose your user for count of movies usres list -> (0,{len(twoD_list) -1 })')
-# which_user = int(input("Etner your user id : ")) 
-# count  = 0 
-# for movie in twoD_list[which_user] : 
-#     if movie : 
-#         count += 1 
-
-# print("print count of movie user see : " , count)
-
 biggest = 0 
 column = -1 
 # Algorithm for Best Movie 
