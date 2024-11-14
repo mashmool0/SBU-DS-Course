27049e68f12b3abae07a13860a3783bc0a0b6856
Amir Mashayekhi <ap4games2017@gmail.com>
2024-11-14 20:33:37 +0330

    problem1_2 add mul and sub 55 percent

--- HW2_P2/problem1_2.cpp
+++ HW2_P2/problem1_2.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
+#include <map>
 using namespace std;
 
 void bubbleSort(long long tavan_ans[], long long zarib_ans[], int len_max)
 {
     for (int i = 0; i < len_max; i++)
     {
-
         for (int j = 1; j < len_max; j++)
         {
             if (tavan_ans[j] < tavan_ans[j - 1])
@@ -16,6 +16,17 @@ void bubbleSort(long long tavan_ans[], long long zarib_ans[], int len_max)
         }
     }
 }
+bool isZeroResult(long long zarib_ans[], int len_max)
+{
+    for (int i = 0; i < len_max; i++)
+    {
+        if (zarib_ans[i] != 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
 void printArray(long long arr[], int size)
 {
     for (int i = 0; i < size; i++)
@@ -25,9 +36,8 @@ void printArray(long long arr[], int size)
     cout << endl;
 }
 
-void sumTwoArray(long long zarib1[], long long tavan1[], long long zarib2[], long long tavan2[], long long zarib_ans[], long long tavan_ans[], int n, int m, int len_max)
+void sumTwoArray(long long zarib1[], long long tavan1[], long long zarib2[], long long tavan2[], long long zarib_ans[], long long tavan_ans[], int n, int m, int *len_max)
 {
-
     for (int i = 0; i < n; i++)
     {
         bool is_eq = false;
@@ -66,14 +76,92 @@ void sumTwoArray(long long zarib1[], long long tavan1[], long long zarib2[], lon
         }
         else
         {
-            tavan_ans[len_max] = tavan2[i];
-            zarib_ans[len_max] = zarib2[i];
-            len_max += 1;
+            tavan_ans[*len_max] = tavan2[i];
+            zarib_ans[*len_max] = zarib2[i];
+            (*len_max) += 1;
         }
     }
-    bubbleSort(tavan_ans, zarib_ans, len_max);
-    printArray(zarib_ans, len_max);
-    printArray(tavan_ans, len_max);
+    bubbleSort(tavan_ans, zarib_ans, *len_max);
+}
+
+void subTwoArray(long long zarib1[], long long tavan1[], long long zarib2[], long long tavan2[], long long zarib_ans[], long long tavan_ans[], int n, int m, int *len_max)
+{
+    for (int i = 0; i < n; i++)
+    {
+        bool is_eq = false;
+        for (int j = 0; j < m; j++)
+        {
+            if (tavan1[i] == tavan2[j])
+            {
+                zarib_ans[i] = zarib1[i] - zarib2[j];
+                tavan_ans[i] = tavan1[i];
+                is_eq = true;
+                break;
+            }
+        }
+        if (is_eq)
+        {
+            continue;
+        }
+        tavan_ans[i] = tavan1[i];
+        zarib_ans[i] = zarib1[i];
+    }
+    // TODO : Check tavan2 was in tavan ans
+    for (int i = 0; i < m; i++)
+    {
+        int it_was = false;
+        for (int j = 0; j < n; j++)
+        {
+            if (tavan2[i] == tavan_ans[j])
+            {
+                it_was = true;
+                break;
+            }
+        }
+        if (it_was)
+        {
+            continue;
+        }
+        else
+        {
+            tavan_ans[*len_max] = tavan2[i];
+            zarib_ans[*len_max] = zarib2[i] * -1;
+            (*len_max) += 1;
+        }
+    }
+    bubbleSort(tavan_ans, zarib_ans, *len_max);
+}
+
+void mulTowArray(long long zarib1[], long long tavan1[], long long zarib2[], long long tavan2[], long long zarib_ans[], long long tavan_ans[], int n, int m, int *len_max)
+{
+    map<int, long long> result; //----------------
+
+    // Iterate over both polynomials
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            int jam_tavan = tavan1[i] + tavan2[j];
+            long long zarb_zarib = zarib1[i] * zarib2[j];
+
+            // Add to the map, summing coefficients for each unique power
+            result[jam_tavan] += zarb_zarib; //----------------
+        }
+    }
+
+    // Copy results from map to zarib_ans and tavan_ans arrays
+    *len_max = 0;
+    for (const auto &entry : result) //----------------
+    {
+        if (entry.second != 0) // Skip zero coefficients
+        {
+            tavan_ans[*len_max] = entry.first;
+            zarib_ans[*len_max] = entry.second;
+            (*len_max)++;
+        }
+    }
+
+    // Sorting is not required here because map iterates in ascending order of keys by default
 }
 
 int main()
@@ -87,8 +175,8 @@ int main()
     long long tavan1[n];
     long long zarib2[m];
     long long tavan2[m];
-    long long zarib_ans[n + m];
-    long long tavan_ans[n + m];
+    long long zarib_ans[n * m]; //---------------- Adjusted to allow up to n * m terms
+    long long tavan_ans[n * m]; //---------------- Adjusted to allow up to n * m terms
     char tag;
 
     // Take input for first poly
@@ -124,11 +212,50 @@ int main()
     // Take input for operations
     cin >> tag;
 
-    printArray(zarib1, n);
-    printArray(tavan1, n);
-    printArray(zarib2, m);
-    printArray(tavan2, m);
-    sumTwoArray(zarib1, tavan1, zarib2, tavan2, zarib_ans, tavan_ans, n, m, len_max);
+    // Check what is operations
+    if (tag == '+')
+    {
+        sumTwoArray(zarib1, tavan1, zarib2, tavan2, zarib_ans, tavan_ans, n, m, &len_max);
+        if (isZeroResult(zarib_ans, len_max))
+        {
+            // If result is zero, set len_max to 1 and print zero arrays
+            len_max = 1;
+            cout << len_max << endl;
+            cout << "0" << endl;
+            cout << "0" << endl;
+        }
+        else
+        {
+            cout << len_max << endl;
+            printArray(zarib_ans, len_max);
+            printArray(tavan_ans, len_max);
+        }
+    }
+    else if (tag == '-')
+    {
+        subTwoArray(zarib1, tavan1, zarib2, tavan2, zarib_ans, tavan_ans, n, m, &len_max);
+        if (isZeroResult(zarib_ans, len_max))
+        {
+            // If result is zero, set len_max to 1 and print zero arrays
+            len_max = 1;
+            cout << len_max << endl;
+            cout << "0" << endl;
+            cout << "0" << endl;
+        }
+        else
+        {
+            cout << len_max << endl;
+            printArray(zarib_ans, len_max);
+            printArray(tavan_ans, len_max);
+        }
+    }
+    else if (tag == '*')
+    {
+        mulTowArray(zarib1, tavan1, zarib2, tavan2, zarib_ans, tavan_ans, n, m, &len_max);
+        cout << len_max << endl;
+        printArray(zarib_ans, len_max);
+        printArray(tavan_ans, len_max);
+    }
 
     return 0;
 }
